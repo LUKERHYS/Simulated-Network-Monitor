b078532b4b293d6d7cd47e7cef08ac593eef80a5
FKeir <fmkeir@gmail.com>
2020-05-16 17:53:43 +0100

    Add automatic seeding to network server

--- network/Network_device_simulation/initdb.py
+++ network/Network_device_simulation/initdb.py
@@ -3,10 +3,17 @@ import os
 from utils.app_factory import create_app
 from utils.db import db
 
+from models.device import Device
+from seeds.seeds import device_data
+
 app = create_app()
 
 with app.app_context():
     db.drop_all()
     db.create_all()
+    for device in device_data:
+        new_device = Device(**device)
+        db.session.add(new_device)
+    db.session.commit()   
 
 print("Database initialized...")
---
+++ network/Network_device_simulation/seeds/seeds.py
@@ -0,0 +1,30 @@
+device_data = [{
+	"host_name": "DevLaptop",
+	"device_type": "PC",
+	"operating_system": "OSx",
+	"mac_address": "82:0f:0c:0a:5d:59" ,
+	"ip_address": "192.168.1.23",
+	"upload_speed": 1,
+	"download_speed": 22,
+	"active_connection": True
+},
+{
+	"host_name": "MacBook",
+	"device_type": "Laptop",
+	"operating_system": "OSx",
+	"mac_address": "82:0f:1c:0a:5d:59" ,
+	"ip_address": "192.168.1.23",
+	"upload_speed": 0,
+	"download_speed": 0,
+	"active_connection": False
+},
+{
+	"host_name": "SmartPhone",
+	"device_type": "Samsung S8",
+	"operating_system": "Android",
+	"mac_address": "82:12:0c:0a:5d:59" ,
+	"ip_address": "192.168.1.23",
+	"upload_speed": 1,
+	"download_speed": 3,
+	"active_connection": True
+}]
